422d54667a8ab7cbc6d01e76c6b12741b5d76706
MartinBlazekovic7 <martinblazekovic7@gmail.com>
2024-08-19 20:27:30 +0200

    Add libraries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
+import { ButtonModule } from 'primeng/button';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-student-log';
