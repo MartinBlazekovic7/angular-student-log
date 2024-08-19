1de1828fa097135051da3b9a989716c1debfe49d
MartinBlazekovic7 <martinblazekovic7@gmail.com>
2024-08-19 22:04:36 +0200

    Add initial calendar component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 import { routes } from './app.routes';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()],
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    importProvidersFrom(
+      CalendarModule.forRoot({
+        provide: DateAdapter,
+        useFactory: adapterFactory,
+      })
+    ),
+  ],
 };
