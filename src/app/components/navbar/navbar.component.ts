f1738851adbf013359cc75cdb02818f0dc920c85
MartinBlazekovic7 <martinblazekovic7@gmail.com>
2024-08-20 21:38:32 +0200

    Add menu on profile icon click

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -4,18 +4,45 @@ import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
 import { SidebarService } from '../../services/sidebar.service';
 import { AvatarModule } from 'primeng/avatar';
+import { MenuItem } from 'primeng/api';
+import { MenuModule } from 'primeng/menu';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, MenubarModule, ButtonModule, AvatarModule],
+  imports: [
+    CommonModule,
+    MenubarModule,
+    ButtonModule,
+    AvatarModule,
+    MenuModule,
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-  isLoggedIn = false;
+  isLoggedIn = true;
 
   constructor(private sidebarService: SidebarService) {}
+
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Profile',
+        icon: 'pi pi-user',
+      },
+      {
+        label: 'Settings',
+        icon: 'pi pi-cog',
+      },
+      {
+        label: 'Sign out',
+        icon: 'pi pi-sign-out',
+      },
+    ];
+  }
 
   toggleSidebar() {
     this.sidebarService.changeSidebarState(true);
