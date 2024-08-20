ebf5dd3e505902c7045b1b7fdcb7e455dde41c1d
MartinBlazekovic7 <martinblazekovic7@gmail.com>
2024-08-20 14:41:18 +0200

    Create initial navbar and footer

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, MenubarModule, ButtonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+})
+export class NavbarComponent {}
