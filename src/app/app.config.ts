0f1e0fb2ffeadd2679d8233cd3deedc18d4c46f4
MartinBlazekovic7 <martinblazekovic7@gmail.com>
2024-08-21 23:37:26 +0200

    Add angular fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,9 @@ import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 import { routes } from './app.routes';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,6 +18,6 @@ export const appConfig: ApplicationConfig = {
         provide: DateAdapter,
         useFactory: adapterFactory,
       })
-    ),
+    ), provideFirebaseApp(() => initializeApp({"projectId":"student-log-app-4ffef","appId":"1:727835854105:web:57055454ca9dc832ee7188","storageBucket":"student-log-app-4ffef.appspot.com","apiKey":"AIzaSyC2X4kdmMN7t1xIpdnbxm4HKMJZTjmyEc8","authDomain":"student-log-app-4ffef.firebaseapp.com","messagingSenderId":"727835854105"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
   ],
 };
