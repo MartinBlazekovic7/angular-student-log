469964fafe38f04015290cef58ae9bb0846501cf
MartinBlazekovic7 <martinblazekovic7@gmail.com>
2024-08-22 22:39:05 +0200

    Update auth guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,12 +1,18 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
+import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = async (route, state) => {
   const auth = inject(Auth);
   const router = inject(Router);
-  const user = await auth.currentUser;
+  const authService = inject(AuthService);
 
-  const isLoggedIn = !!user;
+  let isLoggedIn: boolean | null = null;
+
+  authService.authStatus$.subscribe((user) => {
+    isLoggedIn = !!user;
+  });
+
   return isLoggedIn ? true : router.createUrlTree(['/']);
 };
